591a86c9d5bd196f560d6e00a70c5b1c0ae62e98
Mishellita <mishel.ruiz@angloamerican.com>
2026-08-10 09:29:36 -0500

    Update app.py

--- app.py
+++ app.py
@@ -22,33 +22,50 @@ menu = st.sidebar.radio(
 
 if menu == "Inicio":
 
-    st.header("Inicio")
-
-    st.info(
-        "Bienvenido al Sistema de Apoyo para Formulación de Compostaje"
-    )
+    st.info("Bienvenido al Sistema")
 
 elif menu == "Nueva Formulación":
 
     st.header("Nueva Formulación")
 
-    st.number_input(
-        "Toneladas de Residuos Orgánicos"
+    fecha = st.date_input("Fecha")
+
+    operador = st.text_input("Operador")
+
+    lote = st.text_input("Código de lote")
+
+    ro = st.number_input(
+        "Residuos Orgánicos (ton)",
+        min_value=0.0
     )
 
-    st.number_input(
-        "Toneladas de Residuos Orgánicos Deshidratados"
+    rod = st.number_input(
+        "Residuos Orgánicos Deshidratados (ton)",
+        min_value=0.0
     )
 
-    st.number_input(
-        "Toneladas de Lodo Deshidratado"
+    ld = st.number_input(
+        "Lodo Deshidratado (ton)",
+        min_value=0.0
     )
 
-    st.number_input(
-        "Toneladas de Cartón"
+    carton = st.number_input(
+        "Cartón (ton)",
+        min_value=0.0
     )
 
-    st.button("Calcular")
+    comentarios = st.text_area("Comentarios")
+
+    if st.button("Guardar Formulación"):
+
+        total = ro + rod + ld + carton
+
+        st.success("Formulación registrada")
+
+        st.metric(
+            "Masa Total",
+            f"{round(total,2)} ton"
+        )
 
 elif menu == "Seguimiento":
 
